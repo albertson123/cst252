f16e11498ef2fae68652f330ff736dcf05169558
Caden Albertson <calbertson@csumb.edu>
2020-09-15 21:49:01 -0700

    completed js and did some html

---
+++ lab5/js/lab.js
@@ -0,0 +1,15 @@
+//  Define Variables
+make = "Volkswagon";
+model = "Bus";
+color = "Sage Green";
+year = 1967;
+
+// calculate
+age = 2019 - 1967;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
