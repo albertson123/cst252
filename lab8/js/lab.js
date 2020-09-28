7740ee806527f91abc0a54cb7b6e9556a95617d3
Caden Albertson <calbertson@csumb.edu>
2020-09-28 01:16:09 -0700

    completed js

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,46 +1,40 @@
 /*Author: Caden Albertson <calbertson@csumb.edu>
-*Created: 21 September
+*Created: 27 September
 *License: Public Domain
 */
 
-//will say my name
-function sayMyName() {
-  return "Caden";
+function firstThing(test) {
+  console.log(test + ": This is the first thing.<br>")
 }
-document.write("My name is " + sayMyName());
-//will reverse my array
-function reverseSort(myArray) {
-    return myArray.sort().reverse();
+function secondThing(test) {
+  console.log(test + ": This is the second thing.<br>")
 }
-var myNumArray = [22, 56, 69, 18, 13, 10];
-console.log(reverseSort(myNumArray));
-
-//will reverse the string phrase I type
-function reverseString(myString){
-    return myString.split('').reverse().join('');
-}
-reverseString('Are you suggesting coconuts migrate');
-document.write(reverseString('Are you suggesting coconuts migrate'));
-
-
-//sortUserName -  a function that takes user input and sorts the letters
-//of their name
-function sortUserName() {
-var userName = window.prompt("Hi! Pleese teel me yoür namé soo I cån ffix iT.");
-console.log("username =", userName);
-
-var nameArray = userName.split('');
-console.log("nameArray =", nameArray);
-
-var nameArraySort = nameArray.sort();
-console.log("nameArraySort =", nameArraySort);
-
-var nameSorted = nameArraySort.join('');
-console.log("nameSprted =", nameSorted);
-
-return nameSorted;
+function thirdThing(test) {
+  console.log(test + ": This is the third thing.<br>")
 }
 
-//Output
-document.writeln("Oh hey, I've fixed your name: ",
-  sortUserName(), "</br>");
+firstThing("TEST 1");
+secondThing("TEST 1");
+thirdThing("TEST 1");
+
+//test 2 - in order
+setTimeout(function() {
+  firstThing("TEST 2");
+}, 0);
+setTimeout(function() {
+  secondThing("TEST 2");
+}, 0);
+setTimeout(function() {
+  thirdThing("TEST 2");
+}, 0);
+
+//test 3 - order is 3, 2, 1
+setTimeout(function() {
+  firstThing("TEST 3");
+}, 3000);
+setTimeout(function() {
+  secondThing("TEST 3");
+}, 2000);
+setTimeout(function() {
+  thirdThing("TEST 3");
+}, 1000);
