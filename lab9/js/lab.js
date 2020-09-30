08725448408dd83133c4662b7d1102bae38ccb4c
Caden Albertson <calbertson@csumb.edu>
2020-09-29 23:34:37 -0700

    udpate js

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -3,5 +3,19 @@
 *License: Public Domain
 */
 
-var new1El = document.getElementById("new1");
+var outputEl =
+document.getElementById("div");
+document.body.appendChild(outputEl);
+
+var new1El =
+document.createElement("p");
+
+new1El.innerHTML = "Rispy Cripsy Chris Pratt";
+document.body.appendChild(new1El);
 new1El.style.color = "red";
+
+var new2El =
+document.createElement("p2");
+new2El.innerHTML = "One does not simply escape the coronavirus";
+new2El.style.color = "blue";
+document.body.appendChild(new2El);
