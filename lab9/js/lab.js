e4b87a6e7c8a2438409bcc611d97c6d535c513bf
Caden Albertson <calbertson@csumb.edu>
2020-09-29 23:47:09 -0700

    fixed

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -2,18 +2,18 @@
 *Created: 29 September 2020
 *License: Public Domain
 */
-
+//first output element
 var outputEl =
 document.createElement("p");
 document.body.appendChild(outputEl);
-
+//editing the HTML of the first element
 var new1El =
 document.createElement("p");
 
 new1El.innerHTML = "Rispy Cripsy Chris Pratt";
 document.body.appendChild(new1El);
 new1El.style.backgroundcolor = "red";
-
+//editing the HTML of the second element
 var new2El =
 document.createElement("p2");
 new2El.innerHTML = "One does not simply escape the coronavirus";
