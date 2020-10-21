dbbf50e6db6edb11d69a6bce7fefc551c2fdcf06
Caden Albertson <calbertson@csumb.edu>
2020-10-20 23:30:16 -0700

    js html start and img

---
+++ lab14/js/lab.js
@@ -0,0 +1,17 @@
+/*Author: Caden Albertson <calbertson@csumb.edu>
+*Created: 27 September
+*License: Public Domain
+*/
+
+function Vehicle(make, model, year, color, extras) {
+  this.make = make;
+	this.model = model;
+  this.year = year;
+  this.color = color;
+  this.extras = extras;
+
+  this.says = function() {
+    return this.make + " from " + this.sound + " that's " + this.year + this.color + " and, in fact, " + this.extras;
+  }
+}
+var dragon = new Vehicle("Dragon", "Westeros", "7 AC", "Quicksilver", "will breathe fire");
