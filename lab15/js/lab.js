ff5705fec8849c9d2c675344e7c3ff480d3b27b0
Caden Albertson <calbertson@csumb.edu>
2020-10-26 15:47:33 -0700

    part 2

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -23,8 +23,23 @@ mycar = new Car("Dragon", "Westeros", "7 AC", "Quicksilver");
 
 // document.getElementById("output").innerHTML += "<p>" + mycar.info() + "</p>";
 
+// ------------------- Task 2: Design an OOP Design for Hangman -------------------
 
+document.getElementById("output").innerHTML += "<p>" + "To view the second task for this lab, click on the link below." + "</p>";
 
+// this code was shared with me from val and is a psudocode for hangman, it is up to step 4 cause after that the it became slightly confusing
 
+      // set up the game
+          // create player
+          // create game board
 
-//use mycar.info() to call the function.
+      // start the game loop [Hangman]
+          // render the board
+          // render alphabet
+          // Player selects letters to play the game
+              // if player guesses letters correctly then game ends
+              // stop looping
+          // Else
+              // Keep looping until Player reaches max amount of tries
+              // End Screen with a big old "You Lose :("
+              // Switch to next player and continue looping
