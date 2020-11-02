80fe19200d357810a8d4c4343c7595b7d7878c4d
Caden Albertson <calbertson@csumb.edu>
2020-11-02 10:56:11 -0800

    update

--- lab17/js/lab.js
+++ lab17/js/lab.js
@@ -1,45 +1,13 @@
-/*Author: Caden Albertson <calbertson@csumb.edu>
-*Created: 25 October
-*License: Public Domain
-*Credit to: Tyler Wilson for his inspiring code recipe, and Val Ramirez for running so I could walk
-*/
+//*Author: Caden Albertson <calbertson@csumb.edu>
+//*Created: 1 November
+//*License: Public Domain
+//*Credit to: Eddie Aleman and Val Ramirez for troubleshooting and showing me the ropes
+$(document).ready(function(){
+  $("button").click(function(){
+    alert("This button has been, heh, klicked.")
+  });
+});
 
-class Car {
-  constructor(brand, model, year, color) {
-    this.carname = brand;
-    this.carmodel = model;
-    this.caryear = year;
-    this.carcolor = color;
-  }
-  info() {
-    return "I have a " + this.carname + " " + this.carmodel + " " + this.caryear + " " + this.carcolor + ".";
-  }
-}
+$("button").css("color", "green");
 
-mycar = new Car("Dragon", "Westeros", "7 AC", "Quicksilver");
-// mycar.info()
-// << "I have a Ford"
-
-
-// document.getElementById("output").innerHTML += "<p>" + mycar.info() + "</p>";
-
-// ------------------- Task 2: Design an OOP Design for Hangman -------------------
-
-document.getElementById("output").innerHTML += "<p>" + "To view the second task for this lab, click on the link below." + "</p>";
-
-// this code was shared with me from val and is a psudocode for hangman, it is up to step 4 cause after that the it became slightly confusing
-
-      // set up the game
-          // create player
-          // create game board
-
-      // start the game loop [Hangman]
-          // render the board
-          // render alphabet
-          // Player selects letters to play the game
-              // if player guesses letters correctly then game ends
-              // stop looping
-          // Else
-              // Keep looping until Player reaches max amount of tries
-              // End Screen with a big old "You Lose :("
-              // Switch to next player and continue looping
+$(.challenge)
