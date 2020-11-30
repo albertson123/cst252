1683c4916da3e5755d1da5fe27fe0e50f8b0722c
Caden Albertson <calbertson@csumb.edu>
2020-11-30 01:11:02 -0800

    lab20 html

--- lab20/js/lab.js
+++ lab20/js/lab.js
@@ -4,42 +4,30 @@
 *Credit to: Tyler Wilson for his inspiring code recipe, and Val Ramirez for running so I could walk
 */
 
-class Car {
-  constructor(brand, model, year, color) {
-    this.carname = brand;
-    this.carmodel = model;
-    this.caryear = year;
-    this.carcolor = color;
-  }
-  info() {
-    return "I have a " + this.carname + " " + this.carmodel + " " + this.caryear + " " + this.carcolor + ".";
-  }
-}
+//The information below will use JSON to allow us to link the comic into our webpage
+  endpoint="https://xkcd.com/info.0.json"
 
-mycar = new Car("Dragon", "Westeros", "7 AC", "Quicksilver");
-// mycar.info()
-// << "I have a Ford"
+  //add event listener
+$("button").click(function(){
+  $.ajax({
+    url:endpoint,
+    type: "GET",
+    dataType : "json",
+  })
+  .done(function( data ) {
+    var imageUrl = data.img;
+    var title = data.title;
+    var alt = data.alt;
+    var html = <div id="imageblock">
+      <h2>$(title)</h2>
+      <img src="${imageUrl}" + title="${alt}"><br>
+    </div>
 
+    console.log("My new html: ", html);
+    $("#output").html(html)
+  })
 
-// document.getElementById("output").innerHTML += "<p>" + mycar.info() + "</p>";
-
-// ------------------- Task 2: Design an OOP Design for Hangman -------------------
-
-document.getElementById("output").innerHTML += "<p>" + "To view the second task for this lab, click on the link below." + "</p>";
-
-// this code was shared with me from val and is a psudocode for hangman, it is up to step 4 cause after that the it became slightly confusing
-
-      // set up the game
-          // create player
-          // create game board
-
-      // start the game loop [Hangman]
-          // render the board
-          // render alphabet
-          // Player selects letters to play the game
-              // if player guesses letters correctly then game ends
-              // stop looping
-          // Else
-              // Keep looping until Player reaches max amount of tries
-              // End Screen with a big old "You Lose :("
-              // Switch to next player and continue looping
+  .fail(function( xhr, status, errorThrown ) {
+    console.log(errorThrown + "Status" + status );
+  })
+})
